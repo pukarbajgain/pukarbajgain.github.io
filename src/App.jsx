c0c4bad986b3b14c5e2804226711f3409fe838df
Pukar Bajgai <pucarbajgai@gmail.com>
2025-04-21 14:53:59 +0545

    Landing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#0B192E]">
+      <h1 className="text-2xl font-mono text-white animate-[bounce_0.5s_infinite]">Something Exciting is Brewing...</h1>
+      
+      
+      
+    </div>
+  )
+}
+
+export default App
