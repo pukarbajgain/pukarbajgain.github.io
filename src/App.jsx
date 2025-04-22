8943a04437f4b2e1b097b1f1c2be72123ba69cc1
Pukar Bajgai <pucarbajgai@gmail.com>
2025-04-22 15:53:24 +0545

    added fews things

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { Typewriter } from "react-simple-typewriter";
 function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-[#0B192E]">
-      <h1 className="text-2xl font-mono text-white animate-[bounce_0.5s_infinite]">Something Exciting is Brewing...</h1>
+return (
+  <div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#0B192E]">
+      <div className="text-base font-mono text-white justify-center sm:text-2xl md:text-3xl">
+        <Typewriter
+          words={[
+            "Something cool is brewing...",
+          ]}
+          loop={20}
+          cursor
+          cursorStyle="_"
+          typeSpeed={150}
+          deleteSpeed={80}
+          delaySpeed={1000}
+        />
+      </div>
+      <div>
+        
+      <p className="text-xs sm:text-sm font-mono text-white justify-center mt-5">
+        with ❤️ by {""}
+        <a
+          href="https://www.instagram.com/_pukarbajgain/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[#64ffda] hover:animate-pulse"
+        >
+          Pukar
+        </a>
       
-      
-      
+      </p>
     </div>
-  )
+    </div>
+  
+  </div>
+);
 }
 
-export default App
+export default App;
